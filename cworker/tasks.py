5b4a09cedf0cf1e280e6aad010f692bfa1392a76
Jekmen1 <jeko.kobakidze@gmail.com>
2024-07-29 15:22:34 +0400

    Configuring Task prioritization(Redis)

--- cworker/tasks.py
+++ cworker/tasks.py
@@ -1,8 +1,22 @@
 from celery import shared_task
+import time
 
 @shared_task
-def task1():
+def tp1(queue='celery'):
+    time.sleep(3)
     return
+
 @shared_task
-def task2():
+def tp1(queue='celery:1'):
+    time.sleep(3)
+    return
+
+@shared_task
+def tp1(queue='celery:2'):
+    time.sleep(3)
+    return
+
+@shared_task
+def tp1(queue='celery:3'):
+    time.sleep(3)
     return
--- dcelery/celeryapp.py
+++ dcelery/celeryapp.py
@@ -12,4 +12,10 @@ app.conf.task_default_routes = {'newapp.tasks.task1': {'queue':'queue1'}, 'newap
 # def add_numbers():
 #     return
 
+app.conf.broker_transport_options = {
+    'priority_steps': list(range(10)),
+    'sep': ':',
+    'queue_order_strategy': 'priority',
+}
+
 app.autodiscover_tasks()
